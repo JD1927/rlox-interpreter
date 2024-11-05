bbaac02e4890638522b656f7a6501cf8496682e9
Juan Aguirre <j1927aguirre@gmail.com>
2024-11-04 21:18:32 -0500

    feat: add token and token types

---
+++ src/token.rs
@@ -0,0 +1,89 @@
+use core::fmt;
+
+#[derive(Debug, Clone)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub lexeme: String,
+    pub literal: LiteralType,
+    pub line: usize,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, lexeme: String, literal: LiteralType, line: usize) -> Token {
+        Token {
+            token_type,
+            lexeme,
+            literal,
+            line,
+        }
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{:?} {} {:?}",
+            self.token_type, self.lexeme, self.literal
+        )
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum LiteralType {
+    String(String),
+    Number(f64),
+    Bool(bool),
+    Nil,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenType {
+    // Single-character Tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or Two Character Tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier,
+    String,
+    Number,
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+    // End of line
+    Eof,
+}
